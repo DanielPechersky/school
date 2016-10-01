869c9509f2a52cf39115b61c3b00992f07d8bdfe
dannyPechersky <danny.pechersky@gmail.com>
2016-09-30 22:24:06 -0400

    renamed leftListEnd to split

--- src/sorts/MergeSort.java
+++ src/sorts/MergeSort.java
@@ -10,17 +10,17 @@ public class MergeSort {
         int dataLength = toIndex-fromIndex;  // length of range
         if (dataLength > 1) {
             // get the splitting point to split the array in half with
-            int leftListEnd = dataLength/2+fromIndex;
+            int split = dataLength/2+fromIndex;
 
             // sort each of the two ranges
-            sort(array, fromIndex, leftListEnd);
-            sort(array, leftListEnd, toIndex);
+            sort(array, fromIndex, split);
+            sort(array, split, toIndex);
 
             int[] unsortedData = array.clone();
 
             // perform merge by separating unsortedData into the two ranges
-            for (int dataIndex = fromIndex, leftListIndex = fromIndex, rightListIndex = leftListEnd; dataIndex < toIndex; dataIndex++)
-                if (leftListIndex < leftListEnd && (rightListIndex >= toIndex || unsortedData[leftListIndex] < unsortedData[rightListIndex])) {
+            for (int dataIndex = fromIndex, leftListIndex = fromIndex, rightListIndex = split; dataIndex < toIndex; dataIndex++)
+                if (leftListIndex < split && (rightListIndex >= toIndex || unsortedData[leftListIndex] < unsortedData[rightListIndex])) {
                     array[dataIndex] = unsortedData[leftListIndex];
                     leftListIndex++;
                 } else {
