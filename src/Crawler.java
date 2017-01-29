3907e8184b94225af5aba693d474b32f1d28c757
dannyPechersky <danny.pechersky@gmail.com>
2017-01-29 18:32:15 -0500

    made crawler recursive

--- src/Crawler.java
+++ src/Crawler.java
@@ -10,6 +10,7 @@ public class Crawler {
     private final List<String> fileTypesSeeking;
     private final LinkedList<URL> toRead;
     private final List<URL> hasRead;
+    private final LinkedList<String> results;
 
     private final HashMap<String, Boolean> cachedRobotsTxt;
 
@@ -30,58 +31,54 @@ public class Crawler {
                 .filter(Objects::nonNull)
                 .collect(Collectors.toCollection(LinkedList::new));
         hasRead = new LinkedList<>();
+        results = new LinkedList<>();
         cachedRobotsTxt = new HashMap<>();
     }
 
     public ArrayList<String> crawl(int count) {
         ArrayList<String> results = new ArrayList<>(count);
         try {
-            while (hasRead.size() < count) {
-                String result = crawlURL(toRead.pop());
-                if (result != null)
-                    results.add(result);
-            }
+            while (hasRead.size() < count)
+                crawlURL(toRead.pop(), count);
         } catch(NoSuchElementException e) {} // toRead is empty
         return results;
     }
 
-    private String crawlURL(URL url) {
+    private void crawlURL(URL url, int count) {
         System.out.print(url);
         hasRead.add(url);
         if (url.getPath().contains(".") &&
                 fileTypesSeeking.contains(url.getPath().substring(url.getPath().indexOf('.')+1))) {
             System.out.println(" +added");
-            return url.toString();
+            results.add(url.toString());
         }
 
         System.out.println();
 
-        try {
-            if (isCrawlAllowed(url)) {
-                try (final BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-                    toRead.addAll(Arrays.stream(
-                            br.lines()
-                                    .collect(Collectors.joining())
-                                    .split("href=\"")
-                    )
-                            .skip(1)
-                            .map(section -> section.substring(0, section.indexOf('"')))
-                            .map(urlString -> {
-                                try {
-                                    return new URL(url, urlString);
-                                } catch (MalformedURLException e) {
-                                    return null;
-                                }
-                            })
-                            .filter(Objects::nonNull)
-                            .filter(this::shouldReadIfAllowed)
-                            .collect(Collectors.toList()));
+        if (hasRead.size() < count)
+            try {
+                if (isCrawlAllowed(url)) {
+                    try (final BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+                        String[] links = br.lines()
+                                .collect(Collectors.joining())
+                                .split("href=\"");
+
+                        Arrays.stream(Arrays.copyOfRange(links, 1, Math.min(count-hasRead.size()+1, links.length)))
+                                .map(section -> section.substring(0, section.indexOf('"')))
+                                .map(urlString -> {
+                                    try {
+                                        return new URL(url, urlString);
+                                    } catch (MalformedURLException e) {
+                                        return null;
+                                    }
+                                })
+                                .filter(Objects::nonNull)
+                                .filter(this::shouldReadIfAllowed)
+                                .forEach(url_ -> crawlURL(url_, count));
+                    }
                 }
-            }
 
-        } catch (IOException e) {}
-
-        return null;
+            } catch (IOException e) {}
     }
 
     private boolean shouldReadIfAllowed(URL url) {
@@ -95,7 +92,7 @@ public class Crawler {
     private boolean isCrawlAllowed(String protocol, String host) {
         final Boolean isAllowed = cachedRobotsTxt.get(host);
         if (isAllowed == null) {
-            try (final BufferedReader br = new BufferedReader(new InputStreamReader(new URL(protocol, host, "robots.txt").openStream()))) {
+            try (final BufferedReader br = new BufferedReader(new InputStreamReader(new URL(protocol, host, "/robots.txt").openStream()))) {
                 String line;
                 while (!br.readLine().equalsIgnoreCase("user-agent: *"));
 
