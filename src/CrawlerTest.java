5b3303f6a0dfeb7157683742d822b29de615bf60
dannyPechersky <danny.pechersky@gmail.com>
2017-01-30 13:29:47 -0500

    fixed args

--- src/CrawlerTest.java
+++ src/CrawlerTest.java
@@ -1,6 +1,6 @@
 public class CrawlerTest {
     public static void main(String[] args) {
-        String[] fileTypesSeeking = {"png,jpg,jpeg,gif,ico"};
+        String[] fileTypesSeeking = {"png","jpg","jpeg","gif","ico"};
         String[] toRead = {"http://conwaylife.com/wiki/Main_Page"};
         int count=500;
 
